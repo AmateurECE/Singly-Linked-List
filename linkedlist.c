d0f97735d6e9cec1d92df512d0a84aeb344a5181
AmateurECE <edtwardy@mtu.edu>
2017-06-05 21:02:01 -0500

    Major code formatting restructure
    Reformatted the code to conform to the style guidelines in
    C_Style.txt (As of this commit not yet uploaded).

--- linkedlist.c
+++ linkedlist.c
@@ -1,27 +1,209 @@
-/*
- * linkedlist.c
+/*******************************************************************************
+ * NAME: linkedlist.c
  *
- * The Test file for the custom implementation of a Linked List
- */
+ * AUTHOR: Ethan D. Twardy
+ *
+ * DESCRIPTION: The file defining the API functions for the types and function
+ *              prototypes in linkedlist.h, and code meant for the testing of
+ *              those functions.
+ *
+ * CREATED: 06/05/17
+ * 
+ * LAST EDITED: 06/05/17
+ ***/
+
+/*******************************************************************************
+ * INCLUDES
+ ***/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #include "linkedlist.h"
 
-static inline void error_exit(int status, char * msg)
+/*******************************************************************************
+ * LOCAL PROTOTYPES
+ ***/
+
+#ifdef CONFIG_DEBUG
+static inline void error_exit(int, char *);
+void destroy(void *);
+#endif
+
+/*******************************************************************************
+ * API FUNCTIONS
+ ***/
+
+/*******************************************************************************
+ * FUNCTION:        list_init
+ *
+ * DESCRIPTION:     Initializes a list pointer.
+ *
+ * ARGUMENTS:       list: (List *) -- the list to be operated on.
+ *                  destroy: (void (*)(void *)) -- the user-defined function for
+ *                           freeing data contained in the list.
+ *
+ * RETURN:          void
+ *
+ * NOTES:           O(1)
+ ***/
+void list_init(List * list, void (*destroy)(void * data))
 {
-  fprintf(stderr, "%s", msg);
-  exit(status);
+
+  list->size = 0;
+  list->destroy = destroy;
+  list->head = NULL;
+  list->tail = NULL;
+
   return;
 }
 
-void destroy(void * data)
+/*******************************************************************************
+ * FUNCTION:        list_insnxt
+ *
+ * DESCRIPTION:     Inserts a new ListElm after the ListElm specified.
+ *
+ * ARGUMENTS:       list: (List *) -- the list to be operated on.
+ *                  node: (ListElm *) -- the reference ListElm; new ListElm is
+ *                        inserted after this node.
+ *                  data: (const void *) -- the data to be placed in the new
+ *                        ListElm.
+ *
+ * RETURN:          int -- -1 for failure, 0 for success.
+ *
+ * NOTES:           O(1)
+ ***/
+int list_insnxt(List * list, ListElm * node, const void * data)
 {
-  free(data);
+  ListElm * new;
+  
+  // Perform null check on allocated memory
+  if ((new = (ListElm *)malloc(sizeof(ListElm))) == NULL)
+    return -1;
+
+  new->data = (void *)data;
+
+  // Handle insertion of element at Head
+  if (node == NULL) {
+    if (list_size(list) == 0)
+      list->tail = new;
+
+    new->next = list->head;
+    list->head = new;
+  
+    // Handle insertion somewhere else
+  } else {
+    if (list_next(node) == NULL)
+      list->tail = new;
+
+    new->next = node->next;
+    node->next = new;
+  }
+  
+  list->size++;
+  return 0;
 }
 
+/*******************************************************************************
+ * FUNCTION:        list_remnxt
+ *
+ * DESCRIPTION:     Removes the element after the ListElm specified.
+ *
+ * ARGUMENTS:       list: (List *) -- the list to be operated on.
+ *                  node: (ListElm *) -- the reference ListElm; old ListElm is
+ *                        removed after this node.
+ *                  data: (void **) -- location for the data to be placed in
+ *                        after removing the node.
+ *
+ * RETURN:          int -- -1 for failure, 0 for success.
+ *
+ * NOTES:           O(1)
+ ***/
+int list_remnxt(List * list, ListElm * node, void ** data)
+{
+  if (list_istail(list, node) || list_size(list) == 0)
+    return -1;
+  
+  ListElm * old;
+
+  // Handle deletion at the head
+  if (node == NULL) {
+    old = list->head;
+    list->head = list->head->next;
+    *data = old->data;
+
+    // Handle deletion somewhere else in the list
+  } else {
+    if (list_istail(list, list_next(node)))
+      list->tail = node;
+
+    old = node->next;
+    node->next = node->next->next;
+    *data = old->data;
+  }
+
+  list->size--;
+  free(old);
+  return 0;
+}
+
+/*******************************************************************************
+ * FUNCTION:        list_traverse
+ *
+ * DESCRIPTION:     Traverses the list from end to end and invokes func().
+ *
+ * ARGUMENTS:       list: (List *) -- the list to be operated on.
+ *                  func: (void (*)(void *)) -- the function to call on
+ *                        each data point.
+ *
+ * RETURN:          void
+ *
+ * NOTES:           O(1)
+ ***/
+void list_traverse(List * list, void (*func)(void *))
+{
+  ListElm * elm;
+  
+  for (elm = list_head(list); elm != NULL; elm = list_next(elm)) {
+    func(elm->data);
+  }
+}
+
+/*******************************************************************************
+ * FUNCTION:        list_dest
+ *
+ * DESCRIPTION:     Clears the memory inhabited by a list and sets all bytes 0.
+ *
+ * ARGUMENTS:       list: (List *) -- the list to be operated on.
+ *
+ * RETURN:          void
+ *
+ * NOTES:           O(n)
+ ***/
+int list_dest(List * list)
+{
+  if (list->destroy == NULL)
+    return -1;
+
+  void * data;
+
+  while (list_size(list) > 0) {
+    if (list_remnxt(list, NULL, (void **)&data) == 0) {
+      list->destroy(data);
+    }
+  }
+  memset(list, 0, sizeof(List));
+  return 0;
+}
+
+
+/*******************************************************************************
+ * MAIN
+ ***/
+
+#ifdef CONFIG_DEBUG
 int main(int argc, char * argv[])
 {  
   List * list;
@@ -97,3 +279,24 @@ int main(int argc, char * argv[])
   free(list);
   return 0;
 }
+#endif
+
+/*******************************************************************************
+ * LOCAL FUNCTIONS
+ ***/
+
+#ifdef CONFIG_DEBUG
+static inline void error_exit(int status, char * msg)
+{
+  fprintf(stderr, "%s", msg);
+  exit(status);
+  return;
+}
+
+void destroy(void * data)
+{
+  free(data);
+}
+#endif
+
+/******************************************************************************/
--- makefile
+++ makefile
@@ -1,7 +1,28 @@
-P=linkedlist
-OBJECTS=
-CFLAGS = -g -Wall -O3
+################################################################################
+# NAME: makefile
+# AUTHOR: Ethan D. Twardy
+# DESCRIPTION: makefile for the executable contained in this directory.
+# CREATED: 06/05/17
+# LAST EDITED: 06/05/17
+################################################################################
+
+P = linkedlist
+OBJECTS = linkedlist.c
+CFLAGS = -Wall -O3
 LDLIBS=
 CC=gcc
 
 $(P): $(OBJECTS)
+	$(CC) $(CFLAGS) -o $(P) $(OBJECTS) $(LDLIBS)
+
+.PHONY: debug clean
+
+CFLAGS_DEBUG = -g -Wall -D CONFIG_DEBUG
+
+debug:
+	$(CC) $(CFLAGS_DEBUG) -o $(P) $(OBJECTS) $(LDLIBS)
+
+clean:
+	rm -rf *.c~
+	rm -rf *.h~
+	rm -rf makefile~
